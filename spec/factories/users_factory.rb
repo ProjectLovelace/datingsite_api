f08aaf494a37d2938bcb500211e7ea7f190249eb
J.Marx <jennyrutledge@gmail.com>
2015-03-12 15:20:28 -0400

    Add user factory.

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -4,11 +4,11 @@ FactoryGirl.define do
     email Faker::Internet.email
     password_digest '$2a$10$iUY.xKqExZ7DhaWVAAh5Q.o/gfUDR5I7iHTp2Y46nmrogYIV504X6'
     token SecureRandom.uuid.gsub(/\-/,'')
-    age
-    location
-    seeking
-    gender
-    languages
-    bio Faker::Loren
+    age Faker::Numbers.between(from = 18, to = 90)
+    location Faker::Address.zip_code
+    seeking {["platonic","romantic","both"].sample}
+    gender {["male","female","other"].sample}
+    languages {["c++","ruby","java","verilog","javascript","php","fortran","haskel","c#","c"].sample(3)}
+    bio Faker::Lorem.paragraph(2)
   end
 end
