6a5a5a4c1ebdfe061ae7f62fd1b3d740834ceeb7
J.Marx <jennyrutledge@gmail.com>
2015-03-12 15:06:04 -0400

    Update spec files.

---
+++ spec/factories/users_factory.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+    factory :user do
+    username Faker::Internet.user_name
+    email Faker::Internet.email
+    password_digest '$2a$10$iUY.xKqExZ7DhaWVAAh5Q.o/gfUDR5I7iHTp2Y46nmrogYIV504X6'
+    token SecureRandom.uuid.gsub(/\-/,'')
+    age
+    location
+    seeking
+    gender
+    languages
+    bio Faker::Loren
+  end
+end
