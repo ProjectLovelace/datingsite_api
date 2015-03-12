915a5f20ea685e3882fa57564fd22b9c66237d66
CaraGrace <carapacifico@gmail.com>
2015-03-12 14:51:12 -0400

    remove accidentl blank language entry

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ User.delete_all
 
 User.create(username: 'Alan', email: 'alan@testing.com', age: 31, location:'02134', seeking:'platonic', gender:'other', languages: ['Ruby', 'PHP', 'Javascript'], bio:'I like pair programming and long walks on the beach')
 
-User.create(username: 'Ada', email: 'ada@lordbyron.com', age: 54, location:'02144', seeking:'platonic', gender:'female', languages: ['Babbage', 'Javascript', 'C++', , 'Ada'], bio:'I want to meet nice people for lunch and talk about Project Euler problems!')
+User.create(username: 'Ada', email: 'ada@lordbyron.com', age: 54, location:'02144', seeking:'platonic', gender:'female', languages: ['Babbage', 'Javascript', 'C++', 'Ada'], bio:'I want to meet nice people for lunch and talk about Project Euler problems!')
 
 User.create(username: 'Grace', email: 'grace@hopper.com', age: 22, location:'02180', seeking:'romantic', gender:'other', languages: ['COBOL', 'PHP', 'Fortran', 'Javascript'], bio:'Looking for a long term and RESTful relationship')
 
