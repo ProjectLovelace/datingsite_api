7cc115676f73a4b05b666e715dd26c976736919d
Charles Talbot <charlietalbot@gmail.com>
2015-03-12 15:42:07 -0400

    add index test, rack cors logic for application controller, user model

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     email Faker::Internet.email
     password_digest '$2a$10$iUY.xKqExZ7DhaWVAAh5Q.o/gfUDR5I7iHTp2Y46nmrogYIV504X6'
     token SecureRandom.uuid.gsub(/\-/,'')
-    age Faker::Numbers.between(from = 18, to = 90)
+    age Faker::Number.between(from = 18, to = 90)
     location Faker::Address.zip_code
     seeking {["platonic","romantic","both"].sample}
     gender {["male","female","other"].sample}
