c998df0953ebcee12bafd4e51c2b303eed56a4aa
Charles Talbot <charlietalbot@gmail.com>
2015-03-13 14:40:30 -0400

    image model, migration, user controller actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_one :profile
+  has_one :survey
+  has_many :images
+  has_secure_password
+
+  before_create :generate_token
+
+  def generate_token
+    return if token.present?
+    begin
+      self.token = SecureRandom.uuid.gsub(/\-/,'')
+    end while self.class.exists?(token: token)
+  end
 end
