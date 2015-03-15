20bd177123a6e0fbb5c83cdbb695ba334414fa0f
CaraGrace <carapacifico@gmail.com>
2015-03-15 12:27:32 -0400

    populate actions in locations_controller, establish has_many relationship to users

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ActiveRecord::Base
+  has_many :users
+  validates :zipcode, presence: true
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313182606) do
+ActiveRecord::Schema.define(version: 20150315134559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 20150313182606) do
   add_index "images", ["profile_id"], name: "index_images_on_profile_id", using: :btree
 
   create_table "locations", force: :cascade do |t|
-    t.string "location"
+    t.string   "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -47,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150313182606) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "token"
+    t.integer  "location_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
