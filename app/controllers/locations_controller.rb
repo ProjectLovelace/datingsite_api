a2ddd6e0182d0a75125d8abd40f2c5ff451d52e4
J.Marx <jennyrutledge@gmail.com>
2015-03-17 16:01:00 -0400

    Add featureimage to profile serializer.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -16,7 +16,8 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.find(params[:id])
-    render json: @location, status: 200
+    @profiles = @location.profiles
+    render json: @profiles, status: 200
   end
 
    def update
@@ -30,7 +31,7 @@ class LocationsController < ApplicationController
 
   private
   def location_params
-    params.require(:location).permit(:zipcode, profiles: [])
+    params.require(:location).permit(:zipcode, profilesp: [])
   end
 
   def authenticate
--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,11 @@
 class ProfileSerializer < ActiveModel::Serializer
-attributes :id, :age, :bio, :seeking, :gender, :languages, :location_id
+attributes :id, :age, :bio, :seeking, :gender, :languages, :location_id, :featureImage
+
+  def featureImage
+    #active model query to select one random one.
+    images = Image.where profile_id:self.id
+    offset = rand(images.length)
+    rand_record = images.offset(offset).first.url
+  end
 
 end
